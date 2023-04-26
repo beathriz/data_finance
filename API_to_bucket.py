24cf66ec8f86c43e94e392c323566f121cd0391c
BeathrizFernandes <58796551+beathriz@users.noreply.github.com>
2023-04-26 13:58:02 -0300

    Update API_to_bucket.py

--- API_to_bucket.py
+++ API_to_bucket.py
@@ -24,7 +24,7 @@ else:
 
 
 # Configura as credenciais do AWS
-s3 = boto3.resource('s3', aws_access_key_id='beathrizdev@gmail.com', aws_secret_access_key='Pedro2Pedro@')
+s3 = boto3.resource('s3', aws_access_key_id='', aws_secret_access_key='')
 
 # Nome do bucket que você deseja usar
 bucket_name = 'datafinance'
